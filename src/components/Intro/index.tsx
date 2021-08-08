0e91d6be1a261b0a733f150190fc5013e0a7d4fb
Emanuel Vitor Rodrigues de Souza <emanuel.v.souza@hotmail.com>
2021-08-08 01:32:41 -0300

    Intro finalizado

--- src/components/Intro/index.tsx
+++ src/components/Intro/index.tsx
@@ -1,5 +1,39 @@
-import { Container } from "./styles";
+import { useEffect, useRef } from "react";
+import { init } from "ityped";
+
+import { Container, Left, Right, Wrapper, Down } from "./styles";
 
 export function Intro() {
-  return <Container id="intro"></Container>;
+  const textRef = useRef<HTMLSpanElement>(null);
+
+  useEffect(() => {
+    init(textRef.current!, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Developer", "Junior"],
+    });
+  }, []);
+
+  return (
+    <Container id="intro">
+      <Left>
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="a man" />
+        </div>
+      </Left>
+      <Right>
+        <Wrapper>
+          <h2>Hi There, I'm</h2>
+          <h1>Emanuel Vitor</h1>
+          <h3>
+            Frontend <span ref={textRef}></span>
+          </h3>
+        </Wrapper>
+        <Down href="#portfolio">
+          <img src="assets/down.png" alt="down" />
+        </Down>
+      </Right>
+    </Container>
+  );
 }
--- src/components/Intro/styles.ts
+++ src/components/Intro/styles.ts
@@ -1,5 +1,89 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  background-color: blue;
+  display: flex;
+  background-color: white;
+`;
+
+export const Left = styled.div`
+  flex: 0.5;
+  overflow: hidden;
+
+  .imgContainer {
+    width: 700px;
+    height: 700px;
+    background-color: crimson;
+    border-radius: 50%;
+    display: flex;
+    align-items: flex-end;
+    justify-content: center;
+    float: right;
+
+    .img {
+      height: 90%;
+    }
+  }
+`;
+
+export const Right = styled.div`
+  flex: 0.5;
+  position: relative;
+`;
+
+export const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  padding-left: 50px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  h1 {
+    color: black;
+    font-size: 60px;
+    margin: 10px 0;
+  }
+  h2 {
+    color: black;
+    font-size: 35px;
+  }
+  h3 {
+    color: black;
+    font-size: 30px;
+
+    span {
+      font-size: inherit;
+      color: crimson;
+    }
+
+    .ityped-cursor {
+      animation: blink 1s infinite;
+    }
+
+    @keyframes blink {
+      50% {
+        opacity: 1;
+      }
+      100% {
+        opacity: 0;
+      }
+    }
+  }
+`;
+
+export const Down = styled.a`
+  position: absolute;
+  bottom: 10px;
+  left: 40%;
+
+  img {
+    width: 30px;
+    animation: arrowBlink 2s infinite;
+  }
+
+  @keyframes arrowBlink {
+    100% {
+      opacity: 0;
+    }
+  }
 `;

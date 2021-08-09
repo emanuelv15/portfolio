4838055e2c84859fc0793464a154d0f37afe315c
Emanuel Vitor Rodrigues de Souza <emanuel.v.souza@hotmail.com>
2021-08-09 12:56:26 -0300

    Testimonials finalizado

--- src/components/Testimonials/index.tsx
+++ src/components/Testimonials/index.tsx
@@ -1,5 +1,29 @@
-import { Container } from "./styles";
+import { testimonials } from "../../services/data";
+
+import { Container, Card } from "./styles";
 
 export function Testimonials() {
-  return <Container id="testimonials"></Container>;
+  return (
+    <Container id="testimonials">
+      <h1>Testimonials</h1>
+      <div className="container">
+        {testimonials.map((testimonial) => (
+          <Card className={testimonial.featured ? "featured" : ""}>
+            <div className="top">
+              <img src="assets/right-arrow.png" className="left" alt="" />
+              <img src={testimonial.img} className="user" alt="" />
+              <img src={testimonial.icon} className="right" alt="" />
+            </div>
+            <div className="center">
+              <h3>{testimonial.desc}</h3>
+            </div>
+            <div className="bottom">
+              <h3>{testimonial.name}</h3>
+              <h4>{testimonial.title}</h4>
+            </div>
+          </Card>
+        ))}
+      </div>
+    </Container>
+  );
 }
--- src/components/Testimonials/styles.ts
+++ src/components/Testimonials/styles.ts
@@ -1,5 +1,90 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  background-color: purple;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h1 {
+    font-size: 50px;
+  }
+
+  h1,
+  h3,
+  h4 {
+    color: black;
+  }
+
+  .container {
+    width: 100%;
+    height: 80%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+export const Card = styled.div`
+  width: 250px;
+  height: 70%;
+  border-radius: 10px;
+  box-shadow: 0px 0px 15px -8px black;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  padding: 20px;
+  transition: all 0.5s ease;
+
+  &.featured {
+    width: 300px;
+    height: 75%;
+    margin: 0 30px;
+  }
+
+  &:hover {
+    transform: scale(1.1);
+  }
+
+  .top {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    img {
+      &.left,
+      &.right {
+        height: 25px;
+      }
+
+      &.user {
+        width: 60px;
+        height: 60px;
+        border-radius: 50%;
+        object-fit: cover;
+        margin: 0 30px;
+      }
+    }
+  }
+
+  .center {
+    padding: 10px;
+    border-radius: 10px;
+    background-color: rgb(250, 244, 245);
+  }
+
+  .bottom {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+
+    h3 {
+      margin-bottom: 5px;
+    }
+
+    h4 {
+      color: gray;
+    }
+  }
 `;

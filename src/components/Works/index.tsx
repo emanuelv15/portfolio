9865549c2330a94d20cda0185fa079351fd8b045
Emanuel Vitor Rodrigues de Souza <emanuel.v.souza@hotmail.com>
2021-08-09 10:31:27 -0300

    Works finalizado

--- src/components/Works/index.tsx
+++ src/components/Works/index.tsx
@@ -1,5 +1,53 @@
-import { Container } from "./styles";
+import { useState } from "react";
+
+import { works } from "../../services/data";
+
+import { Container, Slider } from "./styles";
 
 export function Works() {
-  return <Container id="works"></Container>;
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  function handleClick(way: string) {
+    way === "left"
+      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : works.length - 1)
+      : setCurrentSlide(currentSlide < works.length - 1 ? currentSlide + 1 : 0);
+  }
+
+  return (
+    <Container id="works">
+      <Slider style={{ transform: `translateX(-${currentSlide * 100}vw)` }}>
+        {works.map((item) => (
+          <div className="container">
+            <div className="item">
+              <div className="left">
+                <div className="leftContainer">
+                  <div className="imageContainer">
+                    <img src={item.icon} alt="" />
+                  </div>
+                  <h2>{item.title}</h2>
+                  <p>{item.desc}</p>
+                  <span>Projects</span>
+                </div>
+              </div>
+              <div className="right">
+                <img src={item.img} alt="" />
+              </div>
+            </div>
+          </div>
+        ))}
+      </Slider>
+      <img
+        src="assets/arrow.png"
+        className="arrow left"
+        alt=""
+        onClick={() => handleClick("left")}
+      />
+      <img
+        src="assets/arrow.png"
+        className="arrow right"
+        alt=""
+        onClick={() => handleClick("right")}
+      />
+    </Container>
+  );
 }
